69beae5d930702a834a980c09e4e499b9ec3810e
luxury-pixel <luxurybrandsrl2020@gmail.com>
2026-03-17 18:05:16 +0100

    debug database url

--- db.js
+++ db.js
@@ -1,8 +1,12 @@
 const { Pool } = require("pg");
 
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.on("error", (err) => {
